70da070d986b6ff8f4f6fb02783e86683b3a473c
Ronit-bawankule <ronitbawankule20@gmail.com>
2026-05-26 21:23:13 +0530

    Fixed CORS for deployed frontend

--- src/main/java/com/hospital/hospitalweb/LoginController.java
+++ src/main/java/com/hospital/hospitalweb/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/auth")
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 
 public class LoginController {
 

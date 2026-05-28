9948eec87475b57d8e315ce25fe6fb5940054bf9
Ronit-bawankule <ronitbawankule20@gmail.com>
2026-05-28 14:35:22 +0530

    Fix application filename case

--- src/main/java/com/hospital/hospitalweb/dto/LoginRequest.java
+++ src/main/java/com/hospital/hospitalweb/dto/LoginRequest.java
@@ -2,15 +2,32 @@ package com.hospital.hospitalweb.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
 public class LoginRequest {
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
+    @NotBlank
+    @Email
     private String email;
 
-    @NotBlank(message = "Password is required")
+    @NotBlank
     private String password;
+
+    public LoginRequest() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
